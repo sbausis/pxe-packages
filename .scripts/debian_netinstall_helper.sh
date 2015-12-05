c9f93ddf07e87d73ef40641d18228696f8f6795e
Simon Pascal Baur <sbausis@gmx.net>
2015-12-05 01:13:34 +0100

    cosmetics :)

--- .scripts/debian_netinstall_helper.sh
+++ .scripts/debian_netinstall_helper.sh
@@ -23,10 +23,17 @@ cp -f /tmp/${SUITE}_${ARCH}_netboot/debian-installer/${ARCH}/linux debian/${SUIT
 cp -f /tmp/${SUITE}_${ARCH}_netboot/debian-installer/${ARCH}/boot-screens/txt.cfg /tmp/${SUITE}_${ARCH}_txt.cfg
 rm -Rf /tmp/${SUITE}_${ARCH}_netboot.tar.gz /tmp/${SUITE}_${ARCH}_netboot
 
-LABEL="Debian ${SUITE} ${ARCH} ${INTERFACE} NetInstall"
-KERNEL="debian/${SUITE}/netinst/${ARCH}/${INTERFACE}/linux"
-#KERNEL=$(cat /tmp/${SUITE}_${ARCH}_txt.cfg | grep kernel | sed 's/^.*kernel //' | sed "s|debian-installer/${ARCH}|debian/${SUITE}/netinst/${ARCH}/${INTERFACE}|")
-APPEND=$(cat /tmp/${SUITE}_${ARCH}_txt.cfg | grep append | sed 's/^.*append //' | sed "s|debian-installer/${ARCH}/initrd.gz|debian/${SUITE}/netinst/${ARCH}/${INTERFACE}/initrd.gz|")
+if [ -n "${INTERFACE}" ]; then
+	LABEL="Debian ${SUITE} ${ARCH} ${INTERFACE} NetInstall"
+	KERNEL="debian/${SUITE}/netinst/${ARCH}/${INTERFACE}/linux"
+	#KERNEL=$(cat /tmp/${SUITE}_${ARCH}_txt.cfg | grep kernel | sed 's/^.*kernel //' | sed "s|debian-installer/${ARCH}|debian/${SUITE}/netinst/${ARCH}/${INTERFACE}|")
+	APPEND=$(cat /tmp/${SUITE}_${ARCH}_txt.cfg | grep append | sed 's/^.*append //' | sed "s|debian-installer/${ARCH}/initrd.gz|debian/${SUITE}/netinst/${ARCH}/${INTERFACE}/initrd.gz|")
+else
+	LABEL="Debian ${SUITE} ${ARCH} NetInstall"
+	KERNEL="debian/${SUITE}/netinst/${ARCH}/linux"
+	#KERNEL=$(cat /tmp/${SUITE}_${ARCH}_txt.cfg | grep kernel | sed 's/^.*kernel //' | sed "s|debian-installer/${ARCH}|debian/${SUITE}/netinst/${ARCH}/${INTERFACE}|")
+	APPEND=$(cat /tmp/${SUITE}_${ARCH}_txt.cfg | grep append | sed 's/^.*append //' | sed "s|debian-installer/${ARCH}/initrd.gz|debian/${SUITE}/netinst/${ARCH}/initrd.gz|")
+fi
 
 cat <<EOF > config.txt
 LABEL $LABEL
