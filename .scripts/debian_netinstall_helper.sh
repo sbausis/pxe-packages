0d784d6366448b0a9bda7988e1de6484f6d9b757
Simon Pascal Baur <sbausis@gmx.net>
2015-12-06 18:08:48 +0100

    general update debian_netinstall_helper / added support for different Kernel Names (vmlinuz)

--- .scripts/debian_netinstall_helper.sh
+++ .scripts/debian_netinstall_helper.sh
@@ -1,21 +1,100 @@
+#!/bin/bash
 
-set -x
 set -e
+set -x
+
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+SCRIPTNAME=$(basename "$0")
+
+################################################################################
+## Need Packages
+
+NEEDEDPACKAGES=""
+#if [ -z `which git` ]; then NEEDEDPACKAGES+="git-core "; fi
+if [ -n "${NEEDEDPACKAGES}" ]; then
+	echo "Need ${NEEDEDPACKAGES}, installing them..."
+	apt-get -qq -y install ${NEEDEDPACKAGES}
+fi
+
+################################################################################
+## Need TEMPDIR
+
+TEMPDIR=$(mktemp -d -t ${SCRIPTNAME}.XXXXXXXXXX)
+LOCKFILE=${TEMPDIR}/${SCRIPTNAME}.lock
+[ -f "${LOCKFILE}" ] && echo "ERROR ${LOCKFILE} already exist. !!!" && exit 255
+
+################################################################################
+## Need CleanUp
+
+function clean_up() {
+	
+	#echo "Clean up ..."
+	rm -Rf "${TEMPDIR}"
+	
+	if [ "$1" != "0" ]; then
+		echo "ERROR ..."
+		exit $1
+	else
+		#echo " -> Done ..."
+		exit 0
+	fi
+}
+
+function print_help() {
+	echo "
+${SCRIPTNAME}  version 0.1b
+Copyright (C) 2015 by Simon Baur (sbausis at gmx dot net)
+
+Usage: [MIRROR='MIRROR'] [SUITE='SUITE'] [ARCH='ARCH'] [INTERFACE='INTERFACE'] ${SCRIPTNAME}
+ example: MIRROR=nl SUITE=wheezy ARCH=amd64 INTERFACE=gtk ${SCRIPTNAME}
+
+Environment Variables:
+ MIRROR='MIRROR'             (default:ch)     set MIRROR to your Country-Code for Debian-Mirror
+ SUITE='SUITE'               (default:jessie) set SUITE to download
+ ARCH='ARCH'                 (default:i386)   set ARCH to download
+ INTERFACE='INTERFACE'       (default:)       may be 'gtk' / 'xen'
+"
+}
+
+function help_exit() {
+	print_help
+	clean_up 1
+}
+
+################################################################################
+## Need LOCKFILE
+
+trap "{ clean_up; exit 255; }" SIGHUP SIGINT SIGTERM SIGQUIT
+touch ${LOCKFILE}
+
+################################################################################
+# settings Env
 
 MIRROR=${MIRROR-"ch"}
-SUITE=${SUITE-"wheezy"}
-ARCH=${ARCH-"amd64"}
+SUITE=${SUITE-"jessie"}
+ARCH=${ARCH-"i386"}
 INTERFACE=${INTERFACE-""}
+
+################################################################################
+# Download-URL
 
 MIRROR_URL="http://ftp.${MIRROR}.debian.org/debian"
 SUITE_URL="dists/${SUITE}/main"
-ARCH_URL="installer-${ARCH}/current/images/netboot/${INTERFACE}"
+ARCH_URL="installer-${ARCH}/current/images/netboot"
+[ -n "${INTERFACE}" ] && ARCH_URL+="/${INTERFACE}"
 
 URL="${MIRROR_URL}/${SUITE_URL}/${ARCH_URL}"
 
-mkdir -p debian/${SUITE}/netinst/${ARCH}/${INTERFACE}
+################################################################################
+# download NetBoot-Image
 
 wget -O /tmp/${SUITE}_${ARCH}_netboot.tar.gz ${URL}/netboot.tar.gz
+
+################################################################################
+# checksum NetBoot-Image
 
 DIR=${INTERFACE}
 [ -z "${DIR}" ] && DIR="netboot"
@@ -23,28 +102,63 @@ SHASUM=$(wget -q -O - "${MIRROR_URL}/${SUITE_URL}/installer-${ARCH}/current/imag
 if [ "${SHASUM}" != "$(md5sum /tmp/${SUITE}_${ARCH}_netboot.tar.gz | awk -F" " '{print $1}')" ]; then
 	echo "!!! Checksum mismatch !!!"
 	rm -f /tmp/${SUITE}_${ARCH}_netboot.tar.gz
-	exit 1
+	clean_up 1
 fi
+
+################################################################################
+# extract NetBoot-Image
 
 mkdir -p /tmp/${SUITE}_${ARCH}_netboot
 tar -xzf /tmp/${SUITE}_${ARCH}_netboot.tar.gz -C /tmp/${SUITE}_${ARCH}_netboot
 
-cp -f /tmp/${SUITE}_${ARCH}_netboot/debian-installer/${ARCH}/initrd.gz debian/${SUITE}/netinst/${ARCH}/${INTERFACE}/initrd.gz
-cp -f /tmp/${SUITE}_${ARCH}_netboot/debian-installer/${ARCH}/linux debian/${SUITE}/netinst/${ARCH}/${INTERFACE}/linux
-cp -f /tmp/${SUITE}_${ARCH}_netboot/debian-installer/${ARCH}/boot-screens/txt.cfg /tmp/${SUITE}_${ARCH}_txt.cfg
-rm -Rf /tmp/${SUITE}_${ARCH}_netboot.tar.gz /tmp/${SUITE}_${ARCH}_netboot
+################################################################################
+# working directories
 
-if [ -n "${INTERFACE}" ]; then
-	LABEL="Debian ${SUITE} ${ARCH} ${INTERFACE} NetInstall"
-	KERNEL="debian/${SUITE}/netinst/${ARCH}/${INTERFACE}/linux"
-	#KERNEL=$(cat /tmp/${SUITE}_${ARCH}_txt.cfg | grep kernel | sed 's/^.*kernel //' | sed "s|debian-installer/${ARCH}|debian/${SUITE}/netinst/${ARCH}/${INTERFACE}|")
-	APPEND=$(cat /tmp/${SUITE}_${ARCH}_txt.cfg | grep append | sed 's/^.*append //' | sed "s|debian-installer/${ARCH}/initrd.gz|debian/${SUITE}/netinst/${ARCH}/${INTERFACE}/initrd.gz|")
+SOURCE_DIR="/tmp/${SUITE}_${ARCH}_netboot/debian-installer"
+DEST_DIR="debian/${SUITE}/netinst/${ARCH}"
+[ -n "${INTERFACE}" ] && DEST_DIR+="/${INTERFACE}"
+
+mkdir -p ${DEST_DIR}
+
+################################################################################
+# get kernel
+
+if [ -f "${SOURCE_DIR}/vmlinuz" ]; then
+	LINUX="vmlinuz"
+	cp -f ${SOURCE_DIR}/vmlinuz ${DEST_DIR}/vmlinuz
 else
-	LABEL="Debian ${SUITE} ${ARCH} NetInstall"
-	KERNEL="debian/${SUITE}/netinst/${ARCH}/linux"
-	#KERNEL=$(cat /tmp/${SUITE}_${ARCH}_txt.cfg | grep kernel | sed 's/^.*kernel //' | sed "s|debian-installer/${ARCH}|debian/${SUITE}/netinst/${ARCH}/${INTERFACE}|")
-	APPEND=$(cat /tmp/${SUITE}_${ARCH}_txt.cfg | grep append | sed 's/^.*append //' | sed "s|debian-installer/${ARCH}/initrd.gz|debian/${SUITE}/netinst/${ARCH}/initrd.gz|")
+	LINUX="linux"
+	cp -f ${SOURCE_DIR}/linux ${DEST_DIR}/linux
 fi
+
+[ -z "${LINUX}" ] && clean_up 1
+
+################################################################################
+#get ramdisk
+
+cp -f ${SOURCE_DIR}/initrd.gz ${DEST_DIR}/initrd.gz
+
+################################################################################
+# get config
+
+if [ -f "${SOURCE_DIR}/${ARCH}/boot-screens/txt.cfg" ]; then
+
+	LABEL="Debian ${SUITE} ${ARCH} NetInstall"
+	[ -n "${INTERFACE}" ] && LABEL+=" ${INTERFACE}"
+	KERNEL="${DEST_DIR}/${LINUX}"
+	APPEND=$(cat ${SOURCE_DIR}/${ARCH}/boot-screens/txt.cfg | grep append | sed 's/^.*append //' | sed "s|debian-installer/${ARCH}/initrd.gz|${DEST_DIR}/initrd.gz|")
+
+else
+
+	LABEL="Debian ${SUITE} ${ARCH} NetInstall"
+	[ -n "${INTERFACE}" ] && LABEL+=" ${INTERFACE}"
+	KERNEL="${DEST_DIR}/${LINUX}"
+	APPEND="vga=normal initrd=${DEST_DIR}/initrd.gz --quiet"
+
+fi
+
+################################################################################
+# generate config
 
 CONFIG_FILE="./.${SUITE}_${ARCH}_netboot.config"
 [ -n "${INTERFACE}" ] && CONFIG_FILE="./.${SUITE}_${ARCH}_${INTERFACE}_netboot.config"
@@ -53,8 +167,14 @@ LABEL $LABEL
  KERNEL $KERNEL
  APPEND $APPEND
 EOF
-
 cp -f ${CONFIG_FILE} ./config.txt
-rm -f /tmp/${SUITE}_${ARCH}_txt.cfg config.sh
-exit 0
 
+################################################################################
+# cleanUp
+
+rm -Rf /tmp/${SUITE}_${ARCH}_netboot.tar.gz /tmp/${SUITE}_${ARCH}_netboot config.sh
+
+
+clean_up 0
+
+################################################################################
